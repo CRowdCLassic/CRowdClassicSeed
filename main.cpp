c634091bfcc1d08b29517f6ba8b9db355347be2c
cryptocrowdclub <44811053+cryptocrowdclub@users.noreply.github.com>
2018-11-25 23:22:07 +0200

    Change seed url

--- main.cpp
+++ main.cpp
@@ -400,8 +400,8 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"seed.crowdcoin.site", ""};
-static const string testnet_seeds[] = {"testnet-seed.crowdcoin.site",  ""};
+static const string mainnet_seeds[] = {"seed.crcl.dns-cloud.net", ""};
+static const string testnet_seeds[] = {"testnet-seed.crcl.dns-cloud.net",  ""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
